a21fa0ba5d3c57121b24aaa6f2049816798792ee
César Urroz <cesar04071997@gmail.com>
2025-06-02 00:12:23 -0600

    Seguir con el curso de Astro

---
+++ src/Components/Counter.tsx
@@ -0,0 +1,11 @@
+export const Counter = () => {
+    return (
+        <>
+            <h1>Counter</h1>
+            <h3>Value: </h3>
+
+            <button>+1</button>
+            <button>-1</button>
+        </>
+    )
+}
